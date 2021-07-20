a379382ab097710a366401a9c01315bbf7ad7dc0
Nosakhare Edokpayi <co.nosakhare@gmail.com>
2021-07-20 08:11:09 +0100

    added regex queries

--- regex/1create_table.sql
+++ regex/1create_table.sql
@@ -3,3 +3,11 @@ CREATE TABLE em (
     primary key(id),
     email TEXT
 );
+
+INSERT INTO em (email)
+  values ('csev@umich.edu'),
+  ('coleel@umich.edu'),
+  ('sally @uiuc.edu'),
+  ('ted79@umuc.edu'),
+  ('glen1@apple.com'),
+  ('nbody@apple.com');
