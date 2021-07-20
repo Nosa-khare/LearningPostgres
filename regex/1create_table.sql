b45cbb39ad605fc2f6a70303de0874f79c1a3b3b
Nosakhare Edokpayi <co.nosakhare@gmail.com>
2021-07-20 11:08:22 +0100

    Added tweet table and comments to previous queries

--- regex/1create_table.sql
+++ regex/1create_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE em (
+CREATE TABLE mailbox (
     id SERIAL,
     primary key(id),
     email TEXT
@@ -7,7 +7,21 @@ CREATE TABLE em (
 INSERT INTO em (email)
   values ('csev@umich.edu'),
   ('coleel@umich.edu'),
-  ('sally @uiuc.edu'),
+  ('sally@uiuc.edu'),
   ('ted79@umuc.edu'),
   ('glen1@apple.com'),
-  ('nbody@apple.com');
+  ('nbody@apple.com'),
+  ('amy213@gmail.com');
+
+
+CREATE TABLE twitter (
+  id SERIAL,
+  PRIMARY KEY(id)
+  tweet TEXT
+);
+
+INSERT INTO twitter (tweet)
+  VALUES ('This is #SQL and its #FUN'),
+  ('Everyone should learn #SQL, the time is #NOW'),
+  ("You'd do well to also learn #PYTHON" )
+  
